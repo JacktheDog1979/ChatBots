12d98ea8fba8baa0030909695ff9978326400d7d
JacktheDog <128752360+JacktheDog1979@users.noreply.github.com>
2023-03-28 15:05:05 -0300

    Update PrototipoChatGPT-2.py

--- PrototipoChatGPT-2.py
+++ PrototipoChatGPT-2.py
@@ -30,7 +30,7 @@ os.environ['COLUMNS'] = "10000"
 # Set OpenAI API key
 if not gpt_response:
     gpt_response = "I'm sorry, I couldn't generate a response at this time."
-os.environ['OPENAI_API_KEY'] = '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'
+os.environ['OPENAI_API_KEY'] = ''-------------------OPENAI_API_KEY-----------------''
 
 gpt_response = model.generate(
         input_ids=input_ids, 
@@ -52,9 +52,9 @@ if not gpt_response:
     gpt_response = "I'm sorry, I couldn't generate a response at this time."
 
 # Set OpenAI API key
-os.environ['OPENAI_API_KEY'] = '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'
+os.environ['OPENAI_API_KEY'] = ''-------------------OPENAI_API_KEY-----------------''
 
-chatbot = ChatBot(api_key= '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')
+chatbot = ChatBot(api_key= ''-------------------OPENAI_API_KEY-----------------'')
 
 # Define function to read text from file
 def read_text_from_file(file_path):
